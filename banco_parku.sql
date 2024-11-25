31d42429a235a49043507793833bfdd205c6df5b
jose.demesio <jose.demesio@yahoo.com.br>
2024-11-24 23:14:16 -0300

    Inclusão de novas funcionalidades

--- banco_parku.sql
+++ banco_parku.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: parku
 -- ------------------------------------------------------
--- Server version       8.0.39
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,6 +31,47 @@ CREATE TABLE `configuracoes` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `mensalistas`
+--
+
+DROP TABLE IF EXISTS `mensalistas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mensalistas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `placa` varchar(10) NOT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `data_inicio` date NOT NULL,
+  `valor_mensal` decimal(10,2) NOT NULL,
+  `status` varchar(20) DEFAULT 'Ativo',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `placa` (`placa`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pagamentos_mensalistas`
+--
+
+DROP TABLE IF EXISTS `pagamentos_mensalistas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pagamentos_mensalistas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mensalista_id` int NOT NULL,
+  `data_pagamento` datetime DEFAULT NULL,
+  `data_vencimento` date NOT NULL,
+  `valor_pago` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mensalista_id` (`mensalista_id`),
+  CONSTRAINT `pagamentos_mensalistas_ibfk_1` FOREIGN KEY (`mensalista_id`) REFERENCES `mensalistas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `registros`
 --
 
@@ -46,7 +87,7 @@ CREATE TABLE `registros` (
   `tempo_permanencia` time DEFAULT NULL,
   `valor_total` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -58,4 +99,4 @@ CREATE TABLE `registros` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-15 22:03:18
+-- Dump completed on 2024-11-24 23:13:11
